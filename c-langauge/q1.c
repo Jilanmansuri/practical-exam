fe6709169c8f0326f51797759112b0d2e727c866
Jilanmansuri <jilan.f.mansuri.cg@gmail.com>
2025-12-02 16:21:44 +0530

    Refactor sum calculation to use a single loop

--- c-langauge/q1.c
+++ c-langauge/q1.c
@@ -1,39 +1,17 @@
-#include <stdio.h>
+// // using only one loop 
 
+#include <stdio.h>
 int main() {
     int a[3][3] = {
         {1, 2, 3},
-        {4, 5, 6},   
+        {4, 5, 6}, 
         {7, 8, 9}
     };
-    int sum = 0;
 
-    for(int i = 1; i < 2; i++) {
-        for(int j = 0; j < 3; j++) {
-            sum += a[i][j];
-        }
+    int sum = 0;
+    for(int j = 0; j < 3; j++) {  
+        sum += a[1][j];           
     }
-    
     printf("Sum of second row = %d\n", sum);
     return 0;
 }
-
-
-
-// // using only one loop 
-
-// #include <stdio.h>
-// int main() {
-//     int a[3][3] = {
-//         {1, 2, 3},
-//         {4, 5, 6}, 
-//         {7, 8, 9}
-//     };
-
-//     int sum = 0;
-//     for(int j = 0; j < 3; j++) {  
-//         sum += a[1][j];           
-//     }
-//     printf("Sum of second row = %d\n", sum);
-//     return 0;
-// }
