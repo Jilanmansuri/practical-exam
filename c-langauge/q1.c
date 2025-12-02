97b96226c59fd4c0d4b774bdb0e4a3fddf6960d5
Jilanmansuri <jilan.f.mansuri.cg@gmail.com>
2025-12-02 13:57:22 +0530

    using one loop

--- c-langauge/q1.c
+++ c-langauge/q1.c
@@ -17,3 +17,22 @@ int main() {
     printf("Sum of second row = %d\n", sum);
     return 0;
 }
+
+
+
+// // using only one loop 
+
+// #include <stdio.h>
+// int main() {
+//     int a[3][3] = {
+//         {1, 2, 3},
+//         {4, 5, 6}, 
+//         {7, 8, 9}
+//     };
+//     int sum = 0;
+//     for(int j = 0; j < 3; j++) {  
+//         sum += a[1][j];           
+//     }
+//     printf("Sum of second row = %d\n", sum);
+//     return 0;
+// }
